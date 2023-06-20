39fbc511a1fbe97b5645892d68dfbeb0f211b3c5
agung kencana <agungkencana3101@gmail.com>
2023-06-20 11:13:06 +0700

    menambahkan navigasi media sosial

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+
+
+
 // Import stylesheets
 import './style.css';
 
 // Write Javascript code!
 
 
-function OpenWhatsapp() {
- 
+function OpenWhatsapp()
+{
+	const WhatsApp = "com.whatsapp";
+	const intent = new intent(intent.ACTION_MAIN)
+	intent.setPackage(WhatsApp);
+	startActivity(intent);
 }
+
+ 
